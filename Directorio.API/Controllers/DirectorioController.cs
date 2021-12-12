219dc98c8968787fe2385f41341c13e2c5dc3e01
garabito00 <91238183+garabito00@users.noreply.github.com>
2021-12-12 16:03:32 -0400

    proyecto de backend

---
+++ Directorio.API/Controllers/DirectorioController.cs
@@ -0,0 +1,71 @@
+﻿using Directorio.AppService.Servicios.Interfaz;
+using Directorio.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Directorio.API.Controllers
+{
+    [Route("api/directorio")]
+    [ApiController]
+    public class DirectorioController : ControllerBase
+    {
+        private readonly IDirectorioServicio servicio;
+
+        public DirectorioController(IDirectorioServicio _servicio)
+        {
+            servicio = _servicio;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Persona>>> GetDirectorios()
+        {
+            try
+            {
+                var directorio = await servicio.GetDirectorio();
+                return directorio.ToList();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(404, new { Mensaje = e.Message });
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Persona>> GetRegistro(int id)
+        {
+            try
+            {
+                var registro = await servicio.GetUnRegistro(id);
+                return registro;
+            }
+            catch (Exception e)
+            {
+                return StatusCode(404, new { Mensaje = e.Message });
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AgregarUnRegistro(Persona persona)
+        {
+            await servicio.AgregarUnRegistroAlDirectorio(persona);
+            return StatusCode(201, new { Mensaje = "Se inserto el registro en la base de datos" }); ;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> BorrarRegistro(int id)
+        {
+            try
+            {
+                await servicio.BorrarUnRegistro(id);
+                return StatusCode(200, new { Mensaje = "Se borro el registro de la base de datos" });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(404, new { Mensaje = e.Message });
+            }
+        }
+    }
+}
---
+++ Directorio.API/Extensiones/ExtensionesDeService.cs
@@ -0,0 +1,20 @@
+﻿using Directorio.AppService.Servicios.Interfaz;
+using Directorio.AppService.Servicios.Implementacion;
+using Directorio.Data.Repositorio.Implementacion;
+using Directorio.Data.Repositorio.Interfaz;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Directorio.API.Extensiones
+{
+    public static class ExtensionesDeService
+    {
+        public static void InyeccionesDeDependencia(this IServiceCollection services)
+        {
+            //Inyeccion del repositorio
+            services.AddScoped(typeof(IDirectorioRepositorio), typeof(DirectorioRepositorio));
+
+            //Inyeccion del Servicio
+            services.AddScoped(typeof(IDirectorioServicio), typeof(DirectorioService));
+        }
+    }
+}
